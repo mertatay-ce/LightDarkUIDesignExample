33590db1f745e4b3ddf1a1a02884d91b7e5d8d36
Mert Atay <79770837+mertatay-ce@users.noreply.github.com>
2022-12-15 19:08:34 +0300

    LightDarkUIDesignExample
    Example Design

---
+++ app/src/main/java/com/mert/xamplejava/HomeActivity.java
@@ -0,0 +1,31 @@
+package com.mert.xamplejava;
+
+import android.os.Bundle;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class HomeActivity extends AppCompatActivity {
+    Button btnModeChanger;
+    boolean themeLight = true;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+        /*
+        btnModeChanger = findViewById(R.id.btnModeChanger);
+        btnModeChanger.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(themeLight){
+                    themeLight = false;
+                    getDelegate().setLocalNightMode(AppCompatDelegate.MODE_NIGHT_YES);
+                }else{
+                    themeLight = true;
+                    getDelegate().setLocalNightMode(AppCompatDelegate.MODE_NIGHT_NO);
+                }
+            }
+        });*/
+
+    }
+}
